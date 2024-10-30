ab72a3da0c0a87d0d5cf5a0271c0cfb2557c38c3
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-10-30 12:52:33 +0100

    email routes config

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const nodemailer = require('nodemailer')
 const router = express.Router();
-const nodemailer = require('nodemailer');
 
-// Aquí va tu configuración de Nodemailer
+const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    }
+})
 
 router.post('/', (req, res) => {
     const { name, email, message } = req.body;
-    // Lógica para enviar el correo usando Nodemailer
+    
+    const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_USER, // Reemplaza esto con el correo al que deseas enviar
+        subject: `Nuevo mensaje de ${name}`,
+        text: message,
+        replyTo: email, // Permite responder al correo del remitente
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).send('Error al enviar el correo');
+        }
+        res.status(200).send('Correo enviado con éxito');
+    });
 });
 
 module.exports = router;
